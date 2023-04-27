84f1e7197d3a8322841242fd09e5da5cd08ad0cc
OnlyNumber <98099864+OnlyNumber@users.noreply.github.com>
2023-04-27 17:05:03 +0300

    Update

--- RacingRunner/Assets/Scripts/Road/SpawnPoints.cs
+++ RacingRunner/Assets/Scripts/Road/SpawnPoints.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fusion;
 
-public class FragmentRoad : NetworkBehaviour
+public class SpawnPoints : MonoBehaviour
 {
     [SerializeField]
-    private SpawnPoints[] spawnWaves;
+    public Transform[] spawnPoints;
 }
